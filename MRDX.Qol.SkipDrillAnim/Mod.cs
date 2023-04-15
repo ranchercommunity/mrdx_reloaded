5ef13fb9040bf0ce30d9af8c6fd386da26a56fb8
Kemal Fadillah <fadillzzz@gmail.com>
2023-04-15 04:40:53 -0700

    fix(skip-drill): Fixes soft lock after errantry battle

--- MRDX.Qol.SkipDrillAnim/Mod.cs
+++ MRDX.Qol.SkipDrillAnim/Mod.cs
@@ -2,6 +2,10 @@
 using MRDX.Qol.SkipDrillAnim.Template;
 using Reloaded.Hooks.Definitions;
 using Reloaded.Mod.Interfaces;
+using Reloaded.Memory.SigScan.ReloadedII.Interfaces;
+using System.Diagnostics;
+using Reloaded.Hooks.Definitions.X86;
+using Reloaded.Memory.Sources;
 
 namespace MRDX.Qol.SkipDrillAnim;
 
@@ -42,6 +46,16 @@ public class Mod : ModBase // <= Do not Remove.
 
     private bool _isAutoSkipEnabled;
 
+    [Function(CallingConventions.Cdecl)]
+    private delegate bool IsTrainingWrapper(IntPtr unk1, int unk2, IntPtr unk3);
+
+    private IHook<IsTrainingWrapper>? _wrapperHook;
+
+    private bool _isAtErrantryBattleEnd = false;
+
+    // No idea on how to get this value at run time through sig scanning, but this is the vftable for CModePast
+    private const int _cModePastAddr = 0x00D167D4;
+
     public Mod(ModContext context)
     {
         _modLoader = context.ModLoader;
@@ -54,8 +68,19 @@ public class Mod : ModBase // <= Do not Remove.
         _modLoader.GetController<IHooks>().TryGetTarget(out var hooks);
         hooks!.AddHook<IsTrainingDone>(ShouldSkipTraining).ContinueWith(result => _hook = result.Result?.Activate());
         _input = _modLoader.GetController<IController>();
-    }
 
+        var startupScanner = _modLoader.GetController<IStartupScanner>();
+        startupScanner.TryGetTarget(out var scanner);
+        if (scanner != null)
+        {
+            var thisProcess = Process.GetCurrentProcess();
+            var baseAddress = thisProcess.MainModule.BaseAddress;
+            scanner.AddMainModuleScan(
+                "55 8B EC 8B 45 10 8B 48 2C",
+                result => _wrapperHook = _hooks.CreateHook<IsTrainingWrapper>(Wrapper, baseAddress + result.Offset).Activate()
+            );
+        }
+    }
 
     #region For Exports, Serialization etc.
 
@@ -75,10 +100,37 @@ public class Mod : ModBase // <= Do not Remove.
 
     private bool ShouldSkipTraining(nint self)
     {
+        if (_isAtErrantryBattleEnd == true)
+        {
+            _isAtErrantryBattleEnd = false;
+            return _hook.OriginalFunction(self);
+        }
+
         if (_isAutoSkipEnabled)
             return true;
         if (!_input.TryGetTarget(out var controller))
             return false;
         return (controller.Current.Buttons & (ButtonFlags.Circle | ButtonFlags.Triangle)) != 0;
+    }
+
+    /**
+     * From what I can understand there are two cases where this function gets called:
+     * 1. During an ongoing drill
+     * 2. During the cutscene after an errantry battle
+     *
+     * There may be other cases where this does get called, and we might have to handle it
+     * a similar way we're handling #2.
+     */
+    private bool Wrapper(IntPtr unk1, int unk2, IntPtr unk3)
+    {
+        Memory.Instance.Read<IntPtr>((nuint) unk1, out var unk1Val);
+
+        if (unk1Val == _cModePastAddr)
+        {
+            // Becomes relevant in ShouldSkipTraining
+            _isAtErrantryBattleEnd = true;
+        }
+
+        return _wrapperHook.OriginalFunction(unk1, unk2, unk3);
     }
 }
