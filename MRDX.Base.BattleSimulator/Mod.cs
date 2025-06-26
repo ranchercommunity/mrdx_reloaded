e49648662f360ed3374bf88c0f10d6f0260cc0fc
James Rowe <jroweboy@gmail.com>
2025-06-26 16:49:51 -0400

    Start working on subprocess simulator

--- MRDX.Base.BattleSimulator/Mod.cs
+++ MRDX.Base.BattleSimulator/Mod.cs
@@ -1,5 +1,9 @@
-﻿using MRDX.Base.BattleSimulator.Configuration;
+﻿using System.Diagnostics;
+using System.IO.Pipes;
+using System.Runtime.InteropServices;
+using MRDX.Base.BattleSimulator.Configuration;
 using MRDX.Base.BattleSimulator.Template;
+using MRDX.Base.Mod.Interfaces;
 using Reloaded.Hooks.Definitions;
 using Reloaded.Mod.Interfaces;
 
@@ -10,6 +14,10 @@ namespace MRDX.Base.BattleSimulator;
 /// </summary>
 public class Mod : ModBase // <= Do not Remove.
 {
+    private const string ControlPipeName = "mrdx_reloaded_battle_simulator";
+
+    private readonly string _exepath;
+
     /// <summary>
     ///     Provides access to the Reloaded.Hooks API.
     /// </summary>
@@ -36,6 +44,10 @@ public class Mod : ModBase // <= Do not Remove.
     /// </summary>
     private readonly IMod _owner;
 
+    private readonly bool IsServer;
+
+    private Process? _child;
+
     /// <summary>
     ///     Provides access to this mod's configuration.
     /// </summary>
@@ -49,15 +61,34 @@ public class Mod : ModBase // <= Do not Remove.
         _owner = context.Owner;
         _configuration = context.Configuration;
         _modConfig = context.ModConfig;
+        Logger.SetLogLevel(Logger.LogLevel.Trace);
+        // Debugger.Launch();
 
+        // Check if the named pipe exists already
+        IsServer = !Directory.GetFiles(@"\\.\pipe\").ToList().Contains($@"\\.\pipe\{ControlPipeName}");
 
-        // For more information about this template, please see
-        // https://reloaded-project.github.io/Reloaded-II/ModTemplate/
+        // Try hiding the windows on the client side
+        if (!IsServer)
+        {
+            Logger.Debug("Hiding Console");
+            // FreeConsole();
+            var console = GetConsoleWindow();
+            ShowWindow(console, 0);
+        }
 
-        // If you want to implement e.g. unload support in your mod,
-        // and some other neat features, override the methods in ModBase.
+        var mainModule = Process.GetCurrentProcess().MainModule;
+        if (mainModule == null)
+        {
+            Logger.Error("Cannot get main module process");
+            return;
+        }
 
-        // TODO: Implement some mod logic
+        _exepath = Path.GetDirectoryName(mainModule.FileName);
+
+        var maybeGame = _modLoader.GetController<IGame>();
+        if (maybeGame != null && maybeGame.TryGetTarget(out var game))
+            game.OnMonsterBreedsLoaded.Subscribe(_ =>
+                Task.Run(StartMod));
     }
 
     #region For Exports, Serialization etc.
@@ -69,6 +100,85 @@ public class Mod : ModBase // <= Do not Remove.
 #pragma warning restore CS8618
 
     #endregion
+
+    [DllImport("kernel32.dll")]
+    private static extern IntPtr GetConsoleWindow();
+
+    [DllImport("kernel32.dll")]
+    private static extern bool FreeConsole();
+
+    [DllImport("user32.dll", SetLastError = true)]
+    private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+    [DllImport("user32.dll")]
+    private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+    // public static nint GetMainWindowHandle()
+    // {
+    //     var processes = Process.GetProcesses();
+    //     foreach (var process in processes)
+    //         if (!string.IsNullOrEmpty(process.MainWindowTitle))
+    //             return process.MainWindowHandle;
+    //
+    //     return IntPtr.Zero;
+    // }
+
+    private async Task StartMod()
+    {
+        if (!IsServer)
+        {
+            Logger.Debug("Hiding Window");
+            Process.GetCurrentProcess().Refresh();
+            var handle = Process.GetCurrentProcess().MainWindowHandle;
+            var attempts = 0; // keep trying to hide the window for 2 seconds
+            while (handle == nint.Zero && attempts++ < 20)
+            {
+                handle = Process.GetCurrentProcess().MainWindowHandle;
+                await Task.Delay(100);
+            }
+
+            // ShowWindow(handle, 0); // Hide window
+        }
+
+        if (IsServer)
+            await SetupServer();
+        else
+            await SetupClient();
+    }
+
+    private async Task SetupServer()
+    {
+        // Spawn the reloaded process a second time
+        // var config = IConfig<LoaderConfig>.FromPathOrDefault(Paths.LoaderConfigPath);
+        // var exe = Path.Combine(config.LoaderPath32, "Reloaded-II.exe");
+        // TODO don't hardcode this winky face
+        Logger.Debug("Gonna relaunch!");
+
+        var exe = @"C:\Programs\Reloaded-II\Reloaded-II.exe";
+        var procinfo = new ProcessStartInfo(exe, ["--launch", Path.Combine(_exepath, "MF2.exe")])
+        {
+            WindowStyle = ProcessWindowStyle.Hidden,
+            UseShellExecute = false,
+            WorkingDirectory = Path.GetDirectoryName(exe),
+            CreateNoWindow = true
+        };
+        _child = Process.Start(procinfo);
+        ChildProcessTracker.AddProcess(_child);
+        _child.EnableRaisingEvents = true;
+        _child.Exited += (sender, args) => { Logger.Debug("Simulator child process has perished. sad face"); };
+        var server = new NamedPipeServerStream(ControlPipeName, PipeDirection.InOut);
+        Logger.Debug("Waiting for simulation client to connect!");
+        await server.WaitForConnectionAsync();
+        Logger.Debug("Got connection from client!");
+    }
+
+    private async Task SetupClient()
+    {
+        var client = new NamedPipeClientStream(ControlPipeName);
+        Logger.Debug("Connecting to server");
+        await client.ConnectAsync();
+        Logger.Debug("Got connection to server!");
+    }
 
     #region Standard Overrides
 
