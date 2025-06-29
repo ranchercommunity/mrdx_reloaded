911da005e6154f6e77189335030179ce79b2b78c
ArchbishopDave <ArchbishopDave@gmail.com>
2025-06-29 12:01:51 -0600

    Undine/Gaboo - Madlena
    Name is probably wrong from Google Translate!

--- MRDX.Game.MoreMonsters/Mod.cs
+++ MRDX.Game.MoreMonsters/Mod.cs
@@ -363,16 +363,24 @@ public class Mod : ModBase // <= Do not Remove.
         _songIDMapping.Add( 1262740, breed );
 
         breed = new MMBreed( MonsterGenus.Monol, MonsterGenus.Mock, MonsterGenus.Monol, MonsterGenus.Monol );
-        breed.NewBaseBreed( "Revenge", 280, -70, LifeType.Sustainable,
+        breed.NewBaseBreed( "Revenge", 280, -70, LifeType.Sustainable, // STATS NOT DONE
             105, 110, 175, 175, 130, 70,
             1, 2, 4, 4, 2, 0,
             3, 9, 3, 11, 16 );
         _songIDMapping.Add( 1262743, breed );
 
+        
+        breed = new MMBreed( MonsterGenus.Undine, MonsterGenus.Gaboo, MonsterGenus.Undine, MonsterGenus.Undine );
+        breed.NewBaseBreed( "Madlena", 320, 40, LifeType.Sustainable,
+            120, 105, 65, 105, 125, 50,
+            3, 3, 1, 2, 3, 1,
+            2, 11, 19, 10000001, 2 );
+        _songIDMapping.Add( 1262745, breed );
+
         // TODO : Monster Moves and Battle Specials seem to be non-functioning?
 
         /*Songs to use
-         * 1262745	1262749	1262752	1262762	1262766	1262768	1262770	1262783	1262789	989884
+         * 1262749	1262752	1262762	1262766	1262768	1262770	1262783	1262789	989884
          * 989885 989886 989887 989888 989889  989890 989891 989892 989893 989894 
          * 989895 989896 989897 989898 989899 989900*/
     }
